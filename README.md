c797cee65655ffeb7ddd86498dbcdf362d88a44b
wzh <1711143@mail.nankai.edu.cn>
2024-09-30 20:13:38 +0800

    init email script

---
+++ README.md
@@ -0,0 +1,13 @@
+# Scripts for sending emails using Python
+
+## Content (content.html)
+For each email, we only need to replace `[Name]` in the content with the main recipient's name. The script will automatically replace it with the correct name.
+
+## Main Program (main.py)
+
+Please replace the following variables with your own values:
+- `sender_email`: The email address of the sender.
+- `sender_password`: The password of the sender's email account.
+- `sen2url`: the receiver's email addresses.
+- `names`: the names of the recipients.
+- `attaches`: the attachments of the emails.
--- send.py
+++ send.py
@@ -5,43 +5,19 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication 
 import time
 
-def send(toaddrs, professor_name, fromaddr, password):
+def send(toaddrs, professor_name, fromaddr, password, files):
     content = ""
     with open('content.html', 'r', encoding='utf-8') as f:
         content = f.read()
-
-    cvFile = 'CV_Zhonghan.pdf'
-    pdfApart1 = MIMEApplication(open(cvFile, 'rb').read())
-    pdfApart1.add_header('Content-Disposition', 'attachment', filename=cvFile)
-
-    TranscriptFile1 = 'Transcript_Master.pdf'
-    pdfApart2 = MIMEApplication(open(TranscriptFile1, 'rb').read())
-    pdfApart2.add_header('Content-Disposition', 'attachment', filename=TranscriptFile1)
-
-    TranscriptFile2 = 'Transcript_Undergraduate.pdf'
-    pdfApart3 = MIMEApplication(open(TranscriptFile2, 'rb').read())
-    pdfApart3.add_header('Content-Disposition', 'attachment', filename=TranscriptFile2)
-
-    paperFile = 'LS_NRA_VMCAI2024.pdf'
-    pdfApart4 = MIMEApplication(open(paperFile, 'rb').read())
-    pdfApart4.add_header('Content-Disposition', 'attachment', filename=paperFile)
-
-    paperFile2 = 'clauseSMT.pdf'
-    pdfApart5 = MIMEApplication(open(paperFile2, 'rb').read())
-    pdfApart5.add_header('Content-Disposition', 'attachment', filename=paperFile2)
-
-    RSFile = 'Research_Statement_0827.pdf'
-    pdfApart6 = MIMEApplication(open(RSFile, 'rb').read())
-    pdfApart6.add_header('Content-Disposition', 'attachment', filename=RSFile)
-
+    pdfs = []
+    for file in files:
+        pdfApart = MIMEApplication(open(file, 'rb').read())
+        pdfApart.add_header('Content-Disposition', 'attachment', filename=file)
+        pdfs.append(pdfApart)
     m = MIMEMultipart()
     m.attach(MIMEText(content.replace("[Name]", professor_name), 'html', 'utf-8'))
-    m.attach(pdfApart1)
-    m.attach(pdfApart2)
-    m.attach(pdfApart3)
-    m.attach(pdfApart4)
-    m.attach(pdfApart5)
-    m.attach(pdfApart6)
+    for pdf in pdfs:
+        m.attach(pdf)
     m['Subject'] = 'Application for 25Fall PhD - Zhonghan Wang'
 
     try:
@@ -61,11 +37,19 @@ if __name__ == '__main__':
     names = [
         "prof1", "prof2"
     ]
-    myemail = "your_email@email.com"
-    password = "your_password"
+    attaches = [
+        "CV_Zhonghan.pdf",
+        "Transcript_Master.pdf",
+        "Transcript_Undergraduate.pdf",
+        "LS_NRA_VMCAI2024.pdf",
+        "clauseSMT.pdf",
+        "Research_Statement_0827.pdf"
+    ]
+    sender_email = "your_email@email.com"
+    sender_password = "your_password"
     assert len(send2url) == len(names)
     for i in range(len(send2url)):
-        send([send2url[i]], names[i], myemail, password)
+        send([send2url[i]], names[i], sender_email, sender_password, attaches)
         print("Email sent to", names[i])
         time.sleep(3)
     print("All emails sent successfully!")
